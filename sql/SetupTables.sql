a59646b01bcec7fb799f49446a83e0e737dcdfd3
Titus Cheng <tituscheng@Tituss-MacBook-Pro.local>
2015-07-14 00:44:11 -0700

    Remove comment from SQL

--- sql/SetupTables.sql
+++ sql/SetupTables.sql
@@ -1,63 +1,63 @@
 use research;
 
--- 
--- create table Symptoms
--- (
--- 	SymptomID INTEGER,
---     SymptomName VARCHAR(255),
---     PRIMARY KEY (SymptomID)
--- );
 
--- create table Actions
--- (
--- 	ActionID INTEGER,
---     ActionName VARCHAR(255),
---     PRIMARY KEY (ActionID)
--- )
+create table Symptoms
+(
+	SymptomID INTEGER,
+    SymptomName VARCHAR(255),
+    PRIMARY KEY (SymptomID)
+);
 
--- create table WeeklySymptomSurvey
--- (
--- 	SurveyID INTEGER,
--- 	StartTime DATETIME,
---     EndTime DATETIME,
---     UserID INTEGER,
---     PRIMARY KEY (SurveyID, UserID, StartTime)
--- );
+create table Actions
+(
+	ActionID INTEGER,
+    ActionName VARCHAR(255),
+    PRIMARY KEY (ActionID)
+);
 
--- create table SurveyWeeks
--- (
---     UserID INTEGER,
---     StartDate DATETIME,
---     EndDate DATETIME,
---     SurveyID INTEGER,
---     SurveyType INTEGER,
---     PRIMARY KEY (UserID, StartDate, SurveyType)
--- );
--- 
--- create table Surveys
--- (
--- 	SurveyID INTEGER,
---     UserID INTEGER,
---     SymptomID INTEGER,
---     SymptomStatus BOOLEAN,
---     PRIMARY KEY (SurveyID, UserID, SymptomID)
--- )
+create table WeeklySymptomSurvey
+(
+	SurveyID INTEGER,
+	StartTime DATETIME,
+    EndTime DATETIME,
+    UserID INTEGER,
+    PRIMARY KEY (SurveyID, UserID, StartTime)
+);
 
--- create table Users
--- (
--- 	UserID INTEGER,
---     UserName VARCHAR(50),
---     UserPassword VARCHAR(20),
---     PRIMARY KEY (UserID)
--- );
--- 
--- create table Sessions
--- (
--- 	UserID INTEGER,
---     SessionID VARCHAR(255),
---     LastLoginTime DATETIME,
---     PRIMARY KEY(UserID)
--- );
--- 
+create table SurveyWeeks
+(
+    UserID INTEGER,
+    StartDate DATETIME,
+    EndDate DATETIME,
+    SurveyID INTEGER,
+    SurveyType INTEGER,
+    PRIMARY KEY (UserID, StartDate, SurveyType)
+);
+
+create table Surveys
+(
+	SurveyID INTEGER,
+    UserID INTEGER,
+    SymptomID INTEGER,
+    SymptomStatus BOOLEAN,
+    PRIMARY KEY (SurveyID, UserID, SymptomID)
+);
+
+create table Users
+(
+	UserID INTEGER,
+    UserName VARCHAR(50),
+    UserPassword VARCHAR(20),
+    PRIMARY KEY (UserID)
+);
+
+create table Sessions
+(
+	UserID INTEGER,
+    SessionID VARCHAR(255),
+    LastLoginTime DATETIME,
+    PRIMARY KEY(UserID)
+);
+
 
 
