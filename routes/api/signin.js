8ea9ffd9a79ddf945b6399923232732bf240f0ae
Titus Cheng <tituscheng@Tituss-MacBook-Pro.local>
2015-07-14 00:22:26 -0700

    Please enter the commit message for your changes. Lines starting

--- routes/api/signin.js
+++ routes/api/signin.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password: '',
+    password: 'titus2go1986',
     database: 'research',
 });
 
